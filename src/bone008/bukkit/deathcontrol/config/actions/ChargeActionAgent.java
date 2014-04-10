306ece48413669b27df28259e6bde2cb70fc9fdd
Bone008 <bone008@yahoo.de>
2014-04-10 20:25:06 +0200

    reimplemented deathcontrol.free permission

--- src/bone008/bukkit/deathcontrol/config/actions/ChargeActionAgent.java
+++ src/bone008/bukkit/deathcontrol/config/actions/ChargeActionAgent.java
@@ -1,5 +1,8 @@
 package bone008.bukkit.deathcontrol.config.actions;
 
+import org.bukkit.OfflinePlayer;
+
+import bone008.bukkit.deathcontrol.DeathControl;
 import bone008.bukkit.deathcontrol.config.ActionAgent;
 import bone008.bukkit.deathcontrol.config.ActionResult;
 import bone008.bukkit.deathcontrol.config.DeathContext;
@@ -20,9 +23,15 @@ public class ChargeActionAgent extends ActionAgent {
 
 	@Override
 	public ActionResult execute() {
+		OfflinePlayer victim = context.getVictim();
+
+		// check for free permission
+		if (victim.getPlayer() != null && DeathControl.instance.hasPermission(victim.getPlayer(), DeathControl.PERMISSION_FREE))
+			return ActionResult.STANDARD;
+
 		double cost;
 		if (action.isPercentage) {
-			cost = EconomyUtil.calcCost(context.getVictim().getName(), action.money);
+			cost = EconomyUtil.calcCost(victim.getName(), action.money);
 			if (cost > action.capMax)
 				cost = action.capMax;
 			if (cost < action.capMin)
@@ -34,7 +43,7 @@ public class ChargeActionAgent extends ActionAgent {
 
 		context.setVariable("money-paid", EconomyUtil.formatMoney(cost));
 		context.setVariable("money-paid-raw", cost);
-		return (EconomyUtil.payCost(context.getVictim().getName(), cost) ? ActionResult.STANDARD : ActionResult.FAILED);
+		return (EconomyUtil.payCost(victim.getName(), cost) ? ActionResult.STANDARD : ActionResult.FAILED);
 	}
 
 	@Override
